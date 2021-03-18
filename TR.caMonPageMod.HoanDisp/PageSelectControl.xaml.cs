7d87aea263103d79d21918cdb948cded700440fa
ＴＲ <public@technotter.com>
2021-03-18 18:25:18 +0900

    フォントライセンスに関する表記を追加

--- TR.caMonPageMod.HoanDisp/PageSelectControl.xaml.cs
+++ TR.caMonPageMod.HoanDisp/PageSelectControl.xaml.cs
@@ -11,10 +11,15 @@ namespace TR.caMonPageMod.HoanDisp
 		TopPage TP { get; }
 		static HoanLampState HLampState { get; } = new HoanLampState();
 
+		static readonly string CopyrightNotice_Font = @"本Modでは表示フォントに「源柔ゴシック」(http://jikasei.me/font/genjyuu/) を使用しています。
+Licensed under SIL Open Font License 1.1 (http://scripts.sil.org/OFL)
+© 2015 自家製フォント工房, © 2014, 2015 Adobe Systems Incorporated, © 2015 M+ FONTS PROJECT";
+
 		public PageSelectControl(TopPage tp)
 		{
 			TP = tp;
 			InitializeComponent();
+			CopyrightTB.Text = CopyrightNotice_Font;
 		}
 
 		private void GoHomePushed(object sender, RoutedEventArgs e) => TP.CamonIF.BackToHomeDo();
