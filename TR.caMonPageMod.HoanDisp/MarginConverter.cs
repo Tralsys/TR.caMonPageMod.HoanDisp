59da5227b41037b328364fefdb6ca62c940d730a
ＴＲ <public@technotter.com>
2021-03-20 15:57:08 +0900

    一端のみへの値適用機能を追加

--- TR.caMonPageMod.HoanDisp/MarginConverter.cs
+++ TR.caMonPageMod.HoanDisp/MarginConverter.cs
@@ -7,7 +7,7 @@ namespace TR.caMonPageMod.HoanDisp
 {
 	class MarginConverter : IValueConverter
 	{
-		public enum Direction { None, LR, UD, Each }
+		public enum Direction { None, LR, UD, Each, L, R, U, D }
 		public Direction DirectionSetting { get; set; }
 
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -18,6 +18,10 @@ namespace TR.caMonPageMod.HoanDisp
 				Direction.LR => new Thickness(val, 0, val, 0),
 				Direction.UD => new Thickness(0, val, 0, val),
 				Direction.Each => new Thickness(val),
+				Direction.L => new Thickness(val, 0, 0, 0),
+				Direction.U => new Thickness(0, val, 0, 0),
+				Direction.R => new Thickness(0, 0, val, 0),
+				Direction.D => new Thickness(0, 0, 0, val),
 				_ => new Thickness(0)
 			};
 		}
