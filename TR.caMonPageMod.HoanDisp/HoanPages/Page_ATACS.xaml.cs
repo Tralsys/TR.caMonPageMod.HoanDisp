7526090b5af8a540ad903886c5591824619f3b97
ＴＲ <public@technotter.com>
2021-03-24 01:12:09 +0900

    Add ATACS Page

---
+++ TR.caMonPageMod.HoanDisp/HoanPages/Page_ATACS.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TR.caMonPageMod.HoanDisp
+{
+	/// <summary>
+	/// Page_ATACS.xaml の相互作用ロジック
+	/// </summary>
+	public partial class Page_ATACS : UserControl
+	{
+		public Page_ATACS()
+		{
+			InitializeComponent();
+		}
+	}
+}
